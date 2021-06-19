26dd3a28df8c0632cd80ff8701397b19cb4040f8
Ajay Sharma <86146687+aj20216863@users.noreply.github.com>
2021-06-19 21:56:33 +0530

    Update config.js

--- server/config.js
+++ server/config.js
@@ -1,5 +1,5 @@
 module.exports = {
 
-    databaseURL: process.env.DATABASE_URL || "postgres://ccoenraets@localhost/belgianbeers"
+    databaseURL: process.env.DATABASE_URL || "postgres://ulqwrpflsuwtov:b8d878a02a7fe9261efb07f558c24fab236aa6f666d7421a515af8fca9fd43d4@ec2-3-234-22-132.compute-1.amazonaws.com:5432/de7bt3dctscjil"
 
 };
